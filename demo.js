a953e6980478f3d243bfccdf4609c0b7f701e32c
franswah <jfrancis2795@gmail.com>
2016-07-12 23:44:29 -0700

    Bullets!

--- demo.js
+++ demo.js
@@ -19,15 +19,23 @@ var mouseY = 50;
 var robot = {
   speed: 256,
   x: 50,
+  y: ground,
   h: 30,
   w: 20
 };
 
 var probe = {
   speed: 5,
+  health: 100,
+  radius: 40,
   x: 100,
   y: 100
 };
+
+var bullets = [];
+
+var bulletVel = 500;
+var bulletDam = 5;
 
 var keysDown = {};
 
@@ -42,6 +50,10 @@ addEventListener("keyup", function(e) {
 canvas.addEventListener("mousemove", function(e) {
   mouseX = e.clientX;
   mouseY = e.clientY;
+});
+
+addEventListener("click", function(e) {
+  bullets.push(new bullet());
 });
 
 function vector2d(x1, y1, x2, y2) {
@@ -64,6 +76,12 @@ function vector2d(x1, y1, x2, y2) {
   };
 }
 
+function bullet() {
+  this.x = robot.x;
+  this.y = robot.y;
+  this.vector = new vector2d(this.x, this.y, probe.x, probe.y);
+}
+
 var update = function(modifier) {
   if (37 in keysDown) {
     robot.x -= robot.speed * modifier;
@@ -74,15 +92,45 @@ var update = function(modifier) {
 
   probe.x += (mouseX - probe.x) * probe.speed * modifier;
   probe.y += (mouseY - probe.y) * probe.speed * modifier;
+
+  var size = bullets.length;
+  for (ndx = 0; ndx < size; ndx++) {
+    var bul = bullets.shift();
+    bul.x += bul.vector.normalizeDX() * modifier * bulletVel;
+    bul.y += bul.vector.normalizeDY() * modifier * bulletVel;
+
+    if (bul.x < canvas.width && bul.y < canvas.height && bul.x > 0  && bul.y > 0) {
+      if (bul.x < probe.x + probe.radius && bul.x > probe.x - probe.radius
+          && bul.y < probe.y + probe.radius && bul.y > probe.y - probe.radius) {
+        probe.health -= bulletDam;
+        if (probe.health < 0) probe.health = 0;
+      }
+      else {
+        bullets.push(bul);
+      }
+    }
+  }
 }
 
 var render = function() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#555555";
-  ctx.beginPath();
-  ctx.arc(probe.x, probe.y, 40, 0, 2*Math.PI);
-  ctx.stroke();
   ctx.fillRect(robot.x - robot.w/2, ground - robot.h, robot.w, robot.h);
+  ctx.beginPath();
+  ctx.arc(probe.x, probe.y, probe.radius, 0, 2*Math.PI);
+  ctx.fill();
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.fillStyle = "#33d6ff";
+  ctx.arc(probe.x, probe.y, 35, 0, probe.health / 100 * 2 * Math.PI);
+  ctx.lineTo(probe.x, probe.y);
+  ctx.fill();
+  ctx.stroke();
+
+  ctx.fillStyle = "#FF0000";
+  bullets.forEach(function(item) {
+    ctx.fillRect(item.x, item.y, 10, 10);
+  });
 }
 
 var main = function() {
@@ -96,8 +144,6 @@ var main = function() {
 
   requestAnimationFrame(main);
 }
-
-
 
 var then = Date.now();
 main();
